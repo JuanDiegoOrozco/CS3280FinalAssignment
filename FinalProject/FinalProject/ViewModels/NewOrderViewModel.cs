36669435ce00c5ecd2385485b9521ab9a4696ac2
Marauder4294 <marauder42@ymail.com>
2016-08-11 01:07:44 -0600

    Added References and xaml.cs code for the remaining viewmodels

--- FinalProject/FinalProject/ViewModels/NewOrderViewModel.cs
+++ FinalProject/FinalProject/ViewModels/NewOrderViewModel.cs
@@ -9,7 +9,52 @@ using System.Collections.ObjectModel;
 
 namespace FinalAssignment.ViewModels
 {
-    class NewOrderViewModel
+    class NewOrderViewModel : Conductor<IScreen>.Collection.OneActive
     {
+        private ObservableCollection<Order> icollection;
+        public NewOrderViewModel()
+        {
+            this.DisplayName = "Inventory Application";
+
+            //Dummy data
+            Order dummy1 = new Order();
+            dummy1.OrderNumber = 1;
+            dummy1.DatePlaced = DateTime.Today;
+            User dumdum = new User();
+            dumdum.Name = "Mike Jones";
+            dumdum.Phone = "218-330-8004";
+            dumdum.UserId = 1234;
+            dummy1.Purchaser = dumdum;
+            dummy1.TotalCost = 1000000;
+
+            icollection = new ObservableCollection<Order>();
+            icollection.Add(dummy1);
+        }
+        public ObservableCollection<Order> AllOrders
+        {
+            get
+            {
+                return icollection;
+            }
+        }
+        protected override void OnActivate()
+        {
+            base.OnActivate();
+            Orders();
+            foreach (Order od in icollection)
+            {
+                //OrdersViewList.
+            }
+        }
+        public void Orders()
+        {
+            //var ordersVM = IoC.Get<OrdersViewModel>();
+            //IoC.Get<OrdersViewModel>();
+            //OnActivate(ordersVM);
+        }
+        IEnumerable<Order> OrdersView
+        {
+            get { return icollection; }
+        }
     }
 }
--- FinalProject/FinalProject/Views/MainView.xaml.cs
+++ FinalProject/FinalProject/Views/MainView.xaml.cs
@@ -25,7 +25,6 @@ namespace FinalProject.Views
         {
             InitializeComponent();
             DataContext = new MainViewModel();
-
         }
     }
 }
