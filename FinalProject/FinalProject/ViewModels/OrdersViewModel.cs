57cfda4f4952e3c80d7348e384c72beb7c878ef7
Marauder4294 <marauder42@ymail.com>
2016-08-14 15:26:01 -0600

    Revert "Revert "AppBootstrapper working""
    This reverts commit 39eca67462980f4aae13b6c6fd71c4d8bd275f45.

--- FinalProject/FinalProject/ViewModels/OrdersViewModel.cs
+++ FinalProject/FinalProject/ViewModels/OrdersViewModel.cs
@@ -9,8 +9,68 @@ using System.Collections.ObjectModel;
 
 namespace FinalAssignment.ViewModels
 {
-    class OrdersViewModel
+    class OrdersViewModel : Conductor<IScreen>.Collection.OneActive
     {
+        private ObservableCollection<Order> icollection;
+        public OrdersViewModel()
+        {
+            //this.DisplayName = "Orders area";
 
+            //Dummy data
+            Order dummy1 = new Order();
+            dummy1.OrderNumber = 1;
+            dummy1.DatePlaced = DateTime.Today;
+            User dumdum = new User();
+            dumdum.Name = "Mike Jones";
+            dumdum.Phone = "218-330-8004";
+            dumdum.UserId = 1234;
+            dummy1.Purchaser = dumdum;
+            dummy1.TotalCost = 1000000;
+
+            Order dummy2 = new Order();
+            dummy2.OrderNumber = 2;
+            dummy2.DatePlaced = DateTime.Today;
+            User dumdum2 = new User();
+            dumdum2.Name = "50 Cent";
+            dumdum2.Phone = "435-754-8289";
+            dumdum2.UserId = 5678;
+            dummy2.Purchaser = dumdum2;
+            dummy2.TotalCost = .50m;
+
+            icollection = new ObservableCollection<Order>();
+            icollection.Add(dummy1);
+            icollection.Add(dummy2);
+        }
+        public ObservableCollection<Order> AllOrders
+        {
+            get
+            {
+                return icollection;
+            }
+        }
+        //protected override void OnActivate()
+        //{
+        //    base.OnActivate();
+        //    Orders();
+        //}
+        public void Orders()
+        {
+            //var ordersVM = IoC.Get<OrdersViewModel>();
+            //IoC.Get<OrdersViewModel>();
+            //OnActivate(ordersVM);
+        }
+        IEnumerable<Order> OrdersView
+        {
+            get { return icollection; }
+        }
+        /////////////////////////////////////////////////////////////////
+        private ObservableCollection<OrderItem> orderItems;
+        public ObservableCollection<OrderItem> AllItems
+        {
+            get
+            {
+                return orderItems;
+            }
+        }
     }
 }
